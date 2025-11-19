e6cf6ab913a06fe63d7cc8de516a7a9594cbd52d
gaverun <bwaarnell@gmail.com>
2025-11-19 07:47:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
+document.GetElementById("sound").play();
+let language = "english";
+let calculated = "no";
+document.getElementById("id1").style.display = "none";
 function changecolor() {
-  let he = document.getElementById("id1");
-  he.style.color = "black";
+  audio.play();
+  document.getElementById("id1").style.display = "block";
+  calculated = "yes";
+  setTimeout(() => {
+    if (language == "english") {
+      document.getElementById("id1").innerHTML = "yes, noa is gay"
+    } else {
+      document.getElementById("id1").innerHTML = "ja, noa är bög"
+    }
+  }, 1000);
+}
+function changelanguageenglish() {
+  language = "english";
+  document.getElementById("1").innerHTML = "is noa gay?";
+  document.getElementById("button1").innerHTML = "calculate";
+  if (calculated == "yes") {
+    document.getElementById("id1").innerHTML = "yes, noa is gay";
+  } else {
+    document.getElementById("id1").innerHTML = "wait...";
+  }
+}
+
+function changelanguageswedish() {
+  language = "swedish";
+  document.getElementById("1").innerHTML = "är noa bög?";
+  document.getElementById("button1").innerHTML = "räkna ut";
+  if (calculated == "yes") {
+    document.getElementById("id1").innerHTML = "ja, noa är bög";
+  } else {
+    document.getElementById("id1").innerHTML = "vänta...";
+  }
 }
